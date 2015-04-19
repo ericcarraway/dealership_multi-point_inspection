e73d742b0b363e595b4d9e0f6a129a04c118b85a
Eric Lee Carraway <github@ericcarraway.com>
2015-04-19 14:29:51 -0500

    sample json POST to api

--- Dealership.Web/App_Start/WebApiConfig.cs
+++ Dealership.Web/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
 
 namespace Dealership.Web
 {
@@ -16,6 +17,9 @@ namespace Dealership.Web
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Enable WebAPI to return JSON instead of XML
+	        config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/plain"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
