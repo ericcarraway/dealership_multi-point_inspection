abad78e0d3b46ecf95855fb2515c2f0efe03f4b5
Eric Lee Carraway <github@ericcarraway.com>
2015-04-19 13:46:12 -0500

    db migration: 2 new seeds

---
+++ Dealership.Data/Migrations/201504191839038_two_new_seeds.cs
@@ -0,0 +1,16 @@
+namespace Dealership.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class two_new_seeds : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
